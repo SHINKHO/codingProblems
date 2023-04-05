894b77e092c18ede0cc8ab536d3ddfd229f6f760
KYEONGHO SHIN <sinborninkr@gmail.com>
2023-04-05 09:18:25 +0900

    boj10950.c added

---
+++ bojSolutions/boj10950.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int iterno,in1,in2;
+	scanf("%d",&iterno);
+		for(int i=0;i<iterno;i++){
+			scanf("%d %d",&in1,&in2);
+			printf("%d\n",in1+in2);
+		}
+	return 0;
+	
+}
